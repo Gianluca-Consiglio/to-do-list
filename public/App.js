2e4e10e98e2afbd17cb1f50faec23a4f01a346b5
Gianluca-Consiglio <37511000+Gianluca-Consiglio@users.noreply.github.com>
2020-01-08 19:22:50 +0100

    Add CSS and new feature
    Add Bootstrap CSS framework.
    Add remove and stroke feature.

--- public/App.js
+++ public/App.js
@@ -1,34 +1,51 @@
 import React from "react";
-
-import "./App.css";
+import "./index.css";
 const myStorage = window.localStorage;
 
 class Header extends React.Component {
   render() {
-    return <h1 id="titolo">To do list</h1>;
+    return <h4 class="card-title">Awesome Todo list</h4>;
   }
 }
 
 class ToDoInput extends React.Component {
   render() {
     return (
-      <div>
-        <input id="toDoInput" type="text"></input>
-        <button id="saveTodo" onClick={(e) => this.props.addToDo(document.getElementById("toDoInput").value)}>
-          Save
-        </button>
+      <div class="add-items d-flex">
+        <input id="toDoInput" type="text" class="form-control " placeholder="What do you need to do today?"/> <button class="add btn btn-primary font-weight-bold todo-list-add-btn" onClick={(e) => (this.props.addToDo(document.getElementById("toDoInput").value, document.getElementById('toDoInput').value = ''))}>Add</button>
       </div>
     );
   }
 }
 
 class ToDoList extends React.Component {
+  checkToDo(index) {
+    let label = document.getElementById("label" + index)
+    if(label.classList.contains('strike'))
+      label.classList.remove('strike')
+    else
+      label.classList.add('strike')
+  }
+
   render() {
     var elementsList = []
     for(var i = 0; i < parseInt(myStorage.getItem("nElements")); i++){
-      elementsList.push({id:i.toString(),toDo: myStorage.getItem(i.toString())})
-    }  
-    return <ul>{elementsList.map(e => <li key={e.id}>{e.toDo}</li>)}</ul>;
+      if(myStorage.getItem(i.toString()) !== null)
+        elementsList.push({id:i.toString(),toDo: myStorage.getItem(i.toString())})
+    }
+    
+    return (
+      <div class="list-wrapper">
+        <ul class="d-flex flex-column-reverse todo-list">
+        {elementsList.map(e => (
+          <li key={e.id}>
+            <div class="form-check"> <label class="form-check-label" id={"label" + e.id}> <input class="checkbox" type="checkbox" onClick={(event) => this.checkToDo(e.id)}/> {e.toDo} <i class="input-helper"></i></label> </div> <i class="remove mdi mdi-close-circle-outline" onClick={(event) => this.props.removeToDo(e.id)}></i>
+          </li>
+        ))}
+      </ul>
+      </div>
+      
+    )
   }
 }
 
@@ -36,40 +53,48 @@ class Main extends React.Component {
   constructor() {
     super();
     this.state = {
-      elementsList: [
-        { id: 0, toDo: "Wake up" },
-        { id: 1, toDo: "Have breakfast" },
-        { id: 2, toDo: "Brush your teeth" }
-      ],
-      nElements: 3
+      refreshChild : 0
     };
   }
+  
 
   addToDo(param){
     console.log(param)
     if(param === "")
       return
     this.setState({
-      nElements: this.state.nElements + 1,
-      elementsList: [...this.state.elementsList,{id:this.state.nElements,toDo: param}]
+      refreshChild : 1
     })
     if(myStorage.getItem("nElements") === null){
       myStorage.setItem("nElements", "0")
       myStorage.setItem(myStorage.getItem("nElements"),param)
+      myStorage.setItem("nElements", "1")
     }
     else{
-      myStorage.setItem("nElements",(parseInt(myStorage.getItem("nElements")) + 1).toString())
       myStorage.setItem(myStorage.getItem("nElements"),param)
+      myStorage.setItem("nElements",(parseInt(myStorage.getItem("nElements")) + 1).toString())
     }
     }
+
+  removeToDo(index){
+    console.log(index)
+    if(myStorage.getItem(index.toString()) === null){
+      console.log("NULLLLLL")
+      return
+    }
+    myStorage.removeItem(index.toString())
+    myStorage.setItem("nElements",myStorage.getItem("nElements"))
+    this.setState({
+      refreshChild : 1
+    })
+  }
 
   render() {
     return(
       <div>
         <ToDoInput addToDo={event => this.addToDo(event)}/>
-        <ToDoList list={this.state.elementsList} />
+        <ToDoList list={this.state.refreshChild} removeToDo={event => this.removeToDo(event)}/>
       </div>
-      
     )
   }
 }
@@ -77,9 +102,22 @@ class Main extends React.Component {
 function App() {
   return (
     <div>
-      <Header />
-      <Main />
+      <div class="page-content page-container" id="page-content">
+      <div class="padding">
+          <div class="row container d-flex justify-content-center">
+              <div class="col-lg-12">
+                  <div class="card px-3">
+                      <div class="card-body">
+                            <Header />
+                            <Main />
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
     </div>
+    
   );
 }
 
